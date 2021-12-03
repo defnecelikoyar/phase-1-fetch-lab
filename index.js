9911b84fab8917f80bdb2e642d0df2a85feca694
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-12-02 23:47:36 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -10,6 +5,13 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+}
+
+function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => renderBooks(data));
+  return fetch("https://anapioficeandfire.com/api/books")
 }
 
 document.addEventListener('DOMContentLoaded', function() {
